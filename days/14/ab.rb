136e74507f9eea0873a42870a1e876b3b9662af3
Katherine Scherr <45369402+katherinescherr@users.noreply.github.com>
2021-12-14 09:33:30 -0500

    day 14 :dancers:

---
+++ days/14/ab.rb
@@ -0,0 +1,76 @@
+def parse_input(file)
+    template = ""
+    rules = {}
+
+    File.open(file).each do |line|
+        current = line.strip
+        vals = current.split(" -> ")
+        if vals.length == 2
+            rules[vals.first] = vals.last
+        elsif current == ""
+            next
+        else
+            template = current
+        end
+    end
+
+    return template, rules
+end
+
+def initial_pairs(template, rules)
+    pairs = {}
+    rules.each do |pair, _|
+        pairs[pair] = 0
+    end
+    template.scan(/(?=(.{2}))/).flatten.each do |pair|
+        pairs[pair] += 1
+    end
+    pairs
+end
+
+def step(pairs, rules)
+    new_pairs = pairs.clone
+    pairs.each do |pair, value|
+        new_pairs[pair] -= value
+        new_pairs[pair.chars.first + rules[pair]] += value
+        new_pairs[rules[pair] + pair.chars.last] += value
+    end
+    new_pairs
+end
+
+def solve(file, steps)
+    template, rules = parse_input(file)
+    pairs = initial_pairs(template, rules)
+
+    steps.times do
+        pairs = step(pairs, rules)
+    end
+
+    char_counts = {}
+
+    pairs.each do |pair, value|
+        char_counts[pair.chars.first] ||= 0
+        char_counts[pair.chars.first] += value
+        char_counts[pair.chars.last] ||= 0
+        char_counts[pair.chars.last] += value
+    end
+
+    char_counts.each do |key, value|
+        char_counts[key] = (value / 2.0).ceil
+    end
+
+    char_counts.values.max - char_counts.values.min
+end
+
+def solve_a(file)
+    solve(file, 10)
+end
+
+def solve_b(file)
+    solve(file, 40)
+end
+
+puts solve_a("test.txt")
+puts solve_a("input.txt")
+puts solve_b("test.txt")
+puts solve_b("input.txt")
